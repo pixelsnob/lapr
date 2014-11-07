e17113aed0905b4b39fe326474b8d32a4cd60dcf
Luis A. Echeverria <pixelsnob@gmail.com>
2014-11-06 22:25:19 -0800

    Added 404 and moved auth lib to cms

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var
-  port            = 3002,
+  port            = 3003,
   express         = require('express'),
   app             = express(),
   jade_browser    = require('jade-browser'),
@@ -15,8 +15,8 @@ var
   env             = process.env.NODE_ENV || 'development';
 
 require('./lib/db')('lapr');
-require('./lib/auth');
 require('./lib/marked')(app);
+require('cms/lib/auth');
 require('cms/lib/view_helpers')(app);
 
 if (env == 'development') {
---
+++ nav.js
@@ -0,0 +1,34 @@
+
+/**
+ * Site navigation
+ * 
+ *
+ */
+module.exports = {
+  about: {
+    url: '/about',
+    text: 'About'
+  },
+  sounds: {
+    url: '/sounds',
+    text: 'Sounds',
+    items: [
+      {
+        url: '/sounds/solos-and-duos',
+        text: 'Solos & Duos'
+      },
+      {
+        url: '/sounds/trios-and-larger',
+        text: 'Trios & Larger'
+      },
+      {
+        url: '/sounds/wedding-and-acoustic',
+        text: 'Wedding & Acoustic Music'
+      },
+      {
+        url: '/sounds/what-size-band-do-i-choose',
+        text: 'What Size Band Do I Choose?'
+      }
+    ] 
+  }
+};
