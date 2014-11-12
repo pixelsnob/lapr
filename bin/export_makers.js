49b275dc8d3126b2f1c030ec9deef5cbf2e1535e
Luis A. Echeverria <pixelsnob@gmail.com>
2014-11-11 19:36:33 -0800

    More product makers stuff

--- bin/export_makers.js
+++ bin/export_makers.js
@@ -13,15 +13,31 @@ db.connection.on('error', function(e) {
   process.exit(1);
 });
 
-
 async.waterfall([
   function(next) {
     ProductModel.find().distinct('maker', function(err, makers) {
       if (err) {
         return next(err);
       }
-      var out = makers.sort().join("\n");
-      fs.writeFile('./docs/makers.csv', out, function(err) {
+      var out = makers.sort();
+      out = _.uniq(out.map(function(maker) {
+        if (maker.match(/n\/a/i)) {
+          return;
+        }
+        maker = maker.replace(/\s+/g, ' ')
+                     .replace(/^\s+/g, '')
+                     .replace(/\s+$/g, '');
+        if (!maker.length) {
+          return;
+        }
+        var maker_split = maker.split('/');
+        if (maker_split.length == 2) {
+          maker = maker_split[0];
+          makers.push(maker_split[1]);
+        }
+        return maker;
+      }));
+      fs.writeFile('./docs/makers.csv', out.join("\n"), function(err) {
         if (err) {
           return next(err);
         }
