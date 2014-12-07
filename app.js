55a42d86937a9e714a0daaab67c50e264a73d534
Luis A. Echeverria <pixelsnob@gmail.com>
2014-12-07 14:42:46 -0800

    Change to product url again

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.route('/products/categories/:category')
   .get(routes.getProducts)
   .post(routes.getProducts);
 
-app.route('/products/:id')
+app.route('/products/item/:id?')
   .get(routes.getProduct)
   .post(cms_routes.auth, routes.add('Product'))
   .put(cms_routes.auth, routes.update('Product'))
