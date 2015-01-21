c2f8f0728cb5cb62a56251bb9f830cd3ac05b610
Luis A. Echeverria <pixelsnob@gmail.com>
2015-01-20 20:17:58 -0800

    Oops, forgot some files :)

---
+++ public/js/views/product_details_image.js
@@ -0,0 +1,42 @@
+/**
+ * Product details large image popup
+ * 
+ */
+define([
+  'views/base',
+  'template'
+], function(
+  BaseView,
+  template
+) {
+  
+  return BaseView.extend({
+    
+    events: {
+      'click .close':  'closeModal'
+    },
+
+    initialize: function(opts) {
+      this.refs = this.model.collection.refs;
+    },
+    
+    render: function() {
+      this.setElement(template.render('product_details_image', {
+        product: this.model.toJSON() }));
+      this.$el.addClass('product-details-image');
+      return this;
+    },
+    
+    renderModal: function(opts) {
+      this.render();
+      this.$el.modal();
+    },
+
+    closeModal: function() {
+      this.$el.modal('hide');
+      return false;
+    }
+    
+  });
+});
+
