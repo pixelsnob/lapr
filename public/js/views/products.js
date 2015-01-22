b4e769b6f8ed949ea2976418fa5ff5a27c3dc6af
Luis Echeverria <snob@pixelsnob.com>
2015-01-21 21:46:33 -0800

    HUA

--- public/js/views/products.js
+++ public/js/views/products.js
@@ -31,9 +31,9 @@ define([
         });
       }
       this.listenTo(refs.product_categories, 'add remove change',
-        this.render.call(this, false));
+        _.bind(this.render, this, false));
       this.listenTo(refs.makers, 'add remove change',
-        this.render.call(this, false));
+        _.bind(this.render, this, false));
       this.listenTo(refs.filtered_products, 'reset', this.toggleMoreLink);
     },
     
