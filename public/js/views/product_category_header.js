77b178c65fd6c2638dbc46b0777a4d52a615ef6f
Luis A. Echeverria <pixelsnob@gmail.com>
2015-04-03 11:21:57 -0700

    Product category header bug fix, removed 3 col from layout

--- public/js/views/product_category_header.js
+++ public/js/views/product_category_header.js
@@ -14,14 +14,14 @@ define([
     
     initialize: function(opts) {
       this.products = opts.products;
-      this.listenTo(this.products.refs.selected_categories, 'add', this.render);
+      this.listenTo(this.products.refs.selected_categories, 'add reset', this.render);
     },
 
     render: function() {
-      var selected_category = this.products.refs.selected_categories.at(0);
-      if (selected_category) {
-        this.$el.text(selected_category.get('name'));
-      }
+      var selected_category = this.products.refs.selected_categories.at(0),
+          text              = (selected_category ? selected_category.get('name')
+                              : 'All Instruments');
+      this.$el.text(text);
       return this;
     }
 
