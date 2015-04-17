e888b25074094e84dfc2fea28ad26b6b0c7ee9fc
Luis A. Echeverria <pixelsnob@gmail.com>
2015-04-17 11:15:33 -0700

    Modal backdrop space at bottom bug fix (hopefully)

--- public/js/views/modal.js
+++ public/js/views/modal.js
@@ -21,6 +21,12 @@ define([
     render: function(opts) {
       this.$el.find('.modal-body').append(opts.body);
       this.$el.modal();
+      // Attempt to eliminate space at bottom of .modal-backdrop due
+      // to adding content to it dynamically (I guess?)
+      var modal_h = this.$el.find('.modal-content').height() + 100,
+          body_h  = this.$el.height() + 50,
+          h       = (body_h > modal_h ? body_h : modal_h);
+      this.$el.find('.modal-backdrop').height(h);
       return this;
     },
 
