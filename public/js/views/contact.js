672bf2633e7c74077cf385a3eb77afede1365ab0
Luis A. Echeverria <pixelsnob@gmail.com>
2015-07-26 17:13:28 -0700

    Contact page google map tweaks

--- public/js/views/contact.js
+++ public/js/views/contact.js
@@ -37,6 +37,12 @@ define([
       
       var $button = $('<button>').text('Send').addClass('send');
       this.$el.find('form').append($button);
+
+      var $iframe = this.$el.find('.google-map iframe'),
+          src     = 'https://www.google.com/maps/embed/v1/place'            +
+                    '?q=26450+Ruether+Ave.,+%23208+Santa+Clarita,+CA+91350' +
+                    '&key=AIzaSyAjUkPxGyadh5relom7cM9JgAAm7dLa3l8';
+      $iframe.prop('src', src);
       return this;
     },
 
