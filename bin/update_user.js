3ed7bfbf9e187d2dfe08df91e0ced48f092cf648
Luis A. Echeverria <pixelsnob@gmail.com>
2015-08-24 23:12:42 -0700

    Added user update script

---
+++ bin/update_user.js
@@ -0,0 +1,35 @@
+
+'use strict';
+
+var
+  db              = require('../models'),
+  async           = require('async');
+
+db.connection.on('error', function(err) {
+  console.error('mongo error: ' + err);
+  db.connection.close();
+});
+
+db.connection.on('open', function() {
+
+  async.waterfall([
+    function(next) {
+      db.model('User').findOne({ username: { $eq: 'luis' }}, function(err, user) {
+        if (err) {
+          return next(err);
+        }
+        user.password = '12345';
+        user.save(next);
+      });
+    }
+  ], function(err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log('Done');
+    }
+    db.connection.close();
+  });
+
+});
+
