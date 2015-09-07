e6283dc518c082820cfa6b745720f7d76db56ccf
Luis A. Echeverria <pixelsnob@gmail.com>
2015-09-07 16:13:43 -0700

    Visual tweak and main view rendering tweak

--- public/js/views/app.js
+++ public/js/views/app.js
@@ -189,6 +189,8 @@ define([
           products: this.products
         });
         this.$main.html(this.current_view.render().el);
+      } else {
+        this.current_view.render();
       }
       return false;
     }
