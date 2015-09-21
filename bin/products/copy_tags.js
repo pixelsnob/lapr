003aaad147ddcc9c304121c2c87b39089914e215
Luis A. Echeverria <pixelsnob@gmail.com>
2015-09-21 15:11:05 -0700

    Changes to copy tags script

--- bin/products/copy_tags.js
+++ bin/products/copy_tags.js
@@ -19,30 +19,39 @@ db1.products.find().forEach(function(product) {
   db2.products.update({ _id: product._id }, { $set: { tags: product.tags }});
 });
 print(c);
-quit();
+//quit();
 
 c = 0;
 
 // Compare
 db1.products.find().forEach(function(product) {
-  product = _.omit(product, [ '__v', 'hide_sizes_in_lists', 'include_in_slideshow' ]);
+  var keys = [
+    '_id',
+    'name',
+    'description',
+    'alt_names',
+    'sizes'
+  ];
+  product = _.pick(product, keys);
   var staging_product = db2.products.findOne({ _id: product._id });
-  staging_product = _.omit(staging_product, [ '__v', 'hide_sizes_in_lists', 'include_in_slideshow' ]);
+  //staging_product = _.omit(staging_product, [ '__v', 'hide_sizes_in_lists', 'include_in_slideshow' ]);
+  staging_product = _.pick(staging_product, keys);
   if (!_.isEqual(product, staging_product)) {
     print(product.name);
     //var keys = [ 'name', 'description', 'sizes', 'alt_names', 'more_info' ];
-    Object.keys(product).forEach(function(field) {
-      if (!!product[field] && !! staging_product[field] && !_.isEqual(product[field], staging_product[field])) {
+    keys.forEach(function(field) {
+      if (!!product[field] && !!staging_product[field] && !_.isEqual(product[field], staging_product[field])) {
         print('[ ' + field + '1 ]');
         print(product[field]);
         print('[ ' + field + '2 ]');
         print(staging_product[field]);
       }
     });
-    //printjson(product);
-    //printjson(staging_product);
+    printjson(product);
+    printjson(staging_product);
     print('--------------------------------------------------------------------------------');
     c++;
+    if (c > 50) { quit(); }
   }
 });
 
