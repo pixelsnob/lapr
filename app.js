69f3cf92d33219b4d00d16e4731c60e811828c62
Luis Echeverria <pixelsnob@gmail.com>
2016-02-15 13:58:15 -0800

    CSURF latest fix: remove proxy setting. Also, contact form view change

--- app.js
+++ app.js
@@ -34,20 +34,20 @@ app.use(require('cookie-parser')());
 app.use(session({
   store: new redis_store,
   secret: config.session_secret,
-  proxy: true,
-  cookie: { secure: (env == 'production') },
+  proxy: false,
+  cookie: { secure: true },
   resave: true,
   saveUninitialized: true
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-//app.use(require('csurf')());
+app.use(require('csurf')());
 app.use(require('express-paginate').middleware(30, 60));
 app.locals.pretty = true;
 app.locals._ = _;
 
 app.use(function(req, res, next) {
-  //res.locals.csrf = req.csrfToken();
+  res.locals.csrf = req.csrfToken();
   if (req.isAuthenticated()) {
     res.locals.user = _.omit(req.user, [ 'password', '__v' ]);
     // Disable caching if logged in
