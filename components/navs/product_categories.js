b0a61a527dcbf58aed71c7dc984df4749b0a2518
Luis A. Echeverria <pixelsnob@gmail.com>
2016-03-30 19:00:42 -0700

    More components refactoring, cleanup

--- components/navs/product_categories.js
+++ components/navs/product_categories.js
@@ -1,11 +1,20 @@
 
 import React from 'react';
+import ProductCategoryComponent from './product_category';
 
 export default class extends React.Component{
   
   render() {
     return (
-      <div>pc</div>
+      <ul>
+        {this.props.product_categories.map((product_category) => 
+          <ProductCategoryComponent
+            key={product_category._id}
+            product_category={product_category}
+            {...this.props}
+          />
+        )}
+      </ul>
     );
   }
 }
