a3212f63889433799d3827891456274028a82f8f
Luis A. Echeverria <pixelsnob@gmail.com>
2016-04-03 14:16:17 -0700

    Selected tag nav stuff

--- components/navs/tag.js
+++ components/navs/tag.js
@@ -9,6 +9,7 @@ export default class extends React.Component {
   }
 
   getLinkSlugs() {
+    // Work with a copy of selected_tag_ids
     var selected_tag_ids = new Set(this.props.selected_tag_ids);
     if (selected_tag_ids.has(this.props.tag._id)) {
       selected_tag_ids.delete(this.props.tag._id);
@@ -19,10 +20,19 @@ export default class extends React.Component {
       this.props.tags.find(tag => selected_tag_id == tag._id)
     ).map(tag => tag.slug).join(',');
   }
+  
+  isSelected() {
+    return Array.from(this.props.selected_tag_ids).includes(this.props.tag._id);
+  }
 
   render() {
+    var class_names = [];
+    if (this.isSelected()) {
+      class_names.push('selected');
+    }
+
     return (
-      <li>
+      <li className={class_names.join(' ')}>
         <Link to={"/instruments/tags/" + this.getLinkSlugs()}>
           {this.props.tag.name}
         </Link>
