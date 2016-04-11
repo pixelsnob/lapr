3a6b02eff246c48e9b5bdcce444ce5e451b801a0
Luis A. Echeverria <pixelsnob@gmail.com>
2016-04-11 15:35:48 -0700

    Gulpfile image processing change

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,17 @@
 
-const gulp = require('gulp');
-var imageminMozjpeg = require('imagemin-mozjpeg');
+const gulp             = require('gulp');
+const watch            = require('gulp-watch');
+const imageminMozjpeg  = require('imagemin-mozjpeg');
+const imageResize      = require('gulp-image-resize');
 
-//const imageminJpegtran = require('imagemin-jpegtran');
-//const imageminJpegtran = require('');
-
-gulp.task('default', () => {
-  return gulp.src('public/images/**/*.jpg')
-    .pipe(imageminMozjpeg({ quality: 60, progressive: true })())
-    .pipe(gulp.dest('public/dist/images'));
+gulp.task('imagemin', function() {
+  return gulp.src('public/images/products/*.jpg')
+    .pipe(watch('public/images/products/*.jpg'))
+    .pipe(imageminMozjpeg({ quality: 70, progressive: true })())
+    .pipe(imageResize({ width: 400 }))
+    .pipe(gulp.dest('public/dist/images/products/400'))
+    .pipe(imageResize({ width: 140 }))
+    .pipe(gulp.dest('public/dist/images/products/140'));
 });
+
+//return gulp;
