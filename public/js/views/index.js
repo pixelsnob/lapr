6f32603b14bdd135eb942bd801ee466096dd7a26
Luis Echeverria <pixelsnob@gmail.com>
2017-10-22 20:29:37 -0700

    Added onclicks to homepage images; href is set dynamically from first link in content

--- public/js/views/index.js
+++ public/js/views/index.js
@@ -4,13 +4,11 @@
  */
 define([
   'views/base',
-  'views/slideshow',
   'views/content_blocks',
   'template',
   'lib/events'
 ], function(
   BaseView,
-  SlideshowView,
   content_blocks_view,
   template,
   global_events
@@ -19,12 +17,9 @@ define([
   return BaseView.extend({
     
     events: {
-      'click .index-left .content img':  'navigateToFeaturedInstrument',
-      'click .index-left .content a':  'navigateToFeaturedInstrument'
     },
 
     initialize: function(opts) {
-      this.slideshow_view = new SlideshowView;
     },
     
     render: function() {
@@ -32,33 +27,36 @@ define([
       content_blocks_view.setElement(this.$el).render();
       var obj = this;
       content_blocks_view.deferred.done(function() {
-        obj.$featured_instrument = obj.$el.find('[data-name="index-left"] .content a[href^="/instruments"]');
-        if (obj.$featured_instrument.length) {
-          obj.featured_instrument_link = obj.$featured_instrument.attr('href');
-          obj.$featured_instrument.attr('href', '');
-        }
+        // For each column on index page, find the first link in the content, and attach an onlick
+        // to the image that will follow that link
+        [ '.index-left', '.index-middle', '.index-right' ].forEach(function(sel) {
+          var $el = obj.$el.find(sel + ' .content');
+          var $link = $el.find('a:eq(0)');
+          var href = $link.attr('href');
+          if ($link.length && href) {
+            var $img = $el.find('img:eq(0)');
+            if ($img.length) {
+				      $img.click(function() {
+                Backbone.history.navigate(href, true);
+                return false;
+              });
+            }
+            // Make link SPA-friendly, otherwise there will be a page
+            // reload
+            $link.href = 'javascript:void(0)';
+            $link.click(function() {
+              Backbone.history.navigate(href, true);
+              return false;
+            });
+          }
+        });
       });
       global_events.trigger('set-page-title', null);
       $(window).scrollTop(0);
       return this;
     },
 
-    getSlideshowContainer: function() {
-      return this.$el.find('.slideshow-container');
-    },
-    
-    // Pick up instrument link from link in content, if any
-    navigateToFeaturedInstrument: function(ev) {
-      if (this.$featured_instrument.length && this.featured_instrument_link) {
-        Backbone.history.navigate(this.featured_instrument_link, true);
-      }
-      return false;
-    },
-    
     onClose: function() {
-      if (this.slideshow_view) {
-        this.slideshow_view.close();
-      }
     }
 
   });
