ba9dd03a718b4d314c9412ba03763b3a4c3f3567
Luis A. Echeverria <pixelsnob@gmail.com>
2018-05-11 19:58:06 -0700

    Webpack config.js was being affected by .gitignore -- fixed

---
+++ webpack/config.js
@@ -0,0 +1,74 @@
+
+const webpack = require('webpack');
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const app_dir = path.resolve(__dirname, '../');
+
+module.exports = {
+  context: app_dir,
+  entry: { 
+    main: path.join(app_dir, 'public/js/main.js')
+  },
+  output: {
+     path: path.join(app_dir, 'public/dist/js'),
+     publicPath: '/dist/js/',
+     filename: '[name].js'
+  },
+ 	externals: [
+		require('webpack-require-http')
+	],
+  resolve: {
+    modules: [ path.join(app_dir, 'public/js'), 'node_modules' ],
+    alias: {
+      template: 'lib/template',
+      typeahead: 'typeahead.js/dist/typeahead.jquery',
+      youtube: 'lib/youtube',
+      backbone_forms: 'backbone-forms',
+      vex: 'vex-js/js/vex',
+      vex_dialog: 'vex-js/js/vex.dialog'
+    },
+    extensions: [ '.js' ]
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+       'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.ProvidePlugin({
+      $:         'jquery',
+      jQuery:    'jquery',
+      _:         'underscore',
+      Vex:       'vexflow'
+
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [ /node_modules/ ],
+        loader: 'babel-loader'
+      }
+    ]
+  }
+	/*optimization: {
+		splitChunks: {
+			cacheGroups: {
+				main: {
+					//test: /node_modules/,
+					name: 'main',
+					chunks: 'initial'
+          //reuseExistingChunk: true
+				},
+				vendor: {
+					test: /node_modules/,
+					name: 'vendor',
+					chunks: 'initial'
+				}
+			}
+		}
+    //runtimeChunk: { name: 'admin' }
+	},*/
+}
