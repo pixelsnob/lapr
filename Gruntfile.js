77f767a74c0573212c962a257ed703def9553622
Luis A. Echeverria <pixelsnob@gmail.com>
2018-05-12 18:56:34 -0700

    Added grunt task to copy fonts from bootstrap node_modules/dist to public/dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,9 @@ module.exports = function(grunt) {
         },
         files: {
           'public/dist/css/main.css': 'public/less/main.less',
-          'public/dist/css/admin.css': 'public/less/admin.less'
+          'public/dist/css/admin.css': 'public/less/admin.less',
+          'public/dist/css/vex.css': 'node_modules/vex-js/css/vex.css',
+          'public/dist/css/vex-theme-plain.css': 'node_modules/vex-js/css/vex-theme-plain.css'
         }
       }
     },
@@ -22,11 +24,26 @@ module.exports = function(grunt) {
           nospawn: true
         }
       }
-    }
+    },
+		copy: {
+			main: {
+				files: [
+					// includes files within path
+					{
+            expand: true,
+            cwd: 'node_modules/bootstrap/fonts',
+            src: [ '*' ],
+            dest: 'public/dist/fonts/',
+            filter: 'isFile'
+          }
+				]
+			}
+		}
   });
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.registerTask('default', [ 'less' ]);
+  grunt.registerTask('default', [ 'less', 'copy' ]);
 };
 
 
