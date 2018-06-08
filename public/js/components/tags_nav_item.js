ef853e16849965f417c56b3178cec141fd884b71
Luis A. Echeverria <pixelsnob@gmail.com>
2018-06-07 19:06:40 -0700

    Sound search tags navigation, button states, etc.

--- public/js/components/tags_nav_item.js
+++ public/js/components/tags_nav_item.js
@@ -8,31 +8,41 @@ export default class {
     this.store = store;
     this.$el = document.createElement('template');
   }
-  
-  getListElementClassName() {
-    const filtered_products = this.store.refs.filtered_products.models;
-    // Is this tag in the selected_tags collection?
-    const is_selected = this.store.refs.selected_tags.models.find(tag => {
-      return tag.id == this.context.params.tag._id;
-    });
-    if (is_selected) {
-      return 'selected';
-    }
-    // Is this tag assigned to at least one product in the products list?
-    // Is the products list length 1 or bigger?
-    const is_tag_in_filtered_products = filtered_products.find(product => {
-      return product.get('tags').includes(this.context.params.tag._id);
-    });
-    if (!is_tag_in_filtered_products || filtered_products.length <= 1) {
-      return 'disabled';
-    }
-    return '';
-  }
 
   render() {
+    const filtered_products = this.store.refs.filtered_products.models;
+    const selected_tags = this.store.refs.selected_tags.models;
+
+    var class_name = '';
+    var path = '/instruments/tags/';
+
+    const is_selected = selected_tags.find(tag => {
+      return tag.id == this.context.params.tag._id;
+    });
+
+    if (is_selected) {
+      class_name = 'selected';
+      // Remove current slug from path
+      const slugs = selected_tags.filter(tag => tag.id != this.context.params.tag._id)
+      path += slugs.map(tag => tag.get('slug')).join(',');
+    } else {
+      // Not selected (normal state) or disabled
+      const is_tag_in_filtered_products = filtered_products.find(product =>
+        product.get('tags').includes(this.context.params.tag._id)
+      );
+      if (!is_tag_in_filtered_products || filtered_products.length <= 1) {
+        class_name = 'disabled';
+        path = 'javascript:void(0);';
+      } else {
+        // Add current slug to path
+        const slugs = selected_tags.map(tag => tag.get('slug'));
+        path += [ ...slugs, this.context.params.tag.slug ].join(',');
+      }
+    }
     this.$el.innerHTML = template.render('partials/tags_nav_item', {
       tag:  this.context.params.tag,
-      class_name: this.getListElementClassName()
+      class_name,
+      path
     });
     return this.$el.content.cloneNode(true);
   }
