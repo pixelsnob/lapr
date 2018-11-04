27839563d7307c35bd748b56f6b754e664cd9c60
Luis Echeverria <pixelsnob@gmail.com>
2018-11-03 19:07:47 -0700

    Upgrading imagemin-mozjpeg broke the gulp process; switched to gulp-imagemin

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,13 @@
 
 const gulp             = require('gulp');
 const watch            = require('gulp-watch');
-const imageminMozjpeg  = require('imagemin-mozjpeg');
+const imagemin         = require('gulp-imagemin');
 const imageResize      = require('gulp-image-resize');
 
 gulp.task('imagemin', function() {
   return gulp.src('public/images/products/*.{jpg,JPG}')
     .pipe(watch('public/images/products/*.{jpg,JPG}'))
-    .pipe(imageminMozjpeg({ quality: 70, progressive: true })())
+    .pipe(imagemin({ quality: 70, progressive: true }))
     .pipe(imageResize({ width: 400 }))
     .pipe(gulp.dest('public/dist/images/products/400'))
     .pipe(imageResize({ width: 140 }))
